1402b05d2e69cdcfedb0e0dd03c2a4b2e0a0af2f
Wout Mertens <Wout.Mertens@gmail.com>
2016-04-18 19:49:58 +0200

    Autologin as root

--- dongler-vm/csr-dongler.nix
+++ dongler-vm/csr-dongler.nix
@@ -5,6 +5,15 @@
     services.xserver.enable = false;
     i18n.supportedLocales = [ (config.i18n.defaultLocale + "/UTF-8") ];
 
+    # Skip login prompt
+    services.mingetty.autologinUser = "root";
+    users.motd = ''
+      * Plug in the CSR dongle
+      * Switch your TB on a free jump slot
+      * Run pair.sh
+      * Run the make-hid.sh command it outputs. Make sure that is your TextBlade.
+    '';
+
     #  Bluez 5
     hardware.bluetooth.enable = true;
     nixpkgs.config.packageOverrides = pkgs : {
